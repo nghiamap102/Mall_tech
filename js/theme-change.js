cd91bcaf17239298e7073d08ea5e0765382dbdda
nghiamap102 <74520250+nghiamap102@users.noreply.github.com>
2021-09-25 18:04:50 +0700

    a

--- js/theme-change.js
+++ js/theme-change.js
@@ -2,9 +2,9 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 const currentTheme = localStorage.getItem('theme');
 
 document.documentElement.setAttribute('data-theme', currentTheme);
-if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-}
+// if (currentTheme === 'dark') {
+//     toggleSwitch.checked = true;
+// }
 
 function switchTheme(e) {
     if (e.target.checked) {
